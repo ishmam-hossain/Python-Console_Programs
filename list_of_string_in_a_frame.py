4e6009566589d806378d29c260b13ba79ddfe291
Ishmam Hossain <37960094+ishmam-hossain@users.noreply.github.com>
2018-05-05 18:00:17 +0600

    Update list_of_string_in_a_frame.py

--- list_of_string_in_a_frame.py
+++ list_of_string_in_a_frame.py
@@ -20,15 +20,18 @@ def find_longest_word_size(s_list):
     return len(longest_word)
 
 
-s_list = [ "Hello", "World", "in", "a", "frame", "test" ]
-frame_char = '*'
+def putting_in_a_frame(s_list):
+    for i in range(len(s_list)+1):
+        if i == 0 or i == len(s_list):
+            print '*'*(sz+4)
+        else:
+            print('* ' + s_list[i] + ' '*(sz + 1 -len(s_list[i])) + '*'  )
+
+
+
+s_list = [ "Hello", "World", "in", "a", "frame", "test", "another", "new" ]
 
 sz = find_longest_word_size(s_list)
-
-
-for i in range(len(s_list)+2):
-    if i == 1 or i == len(s_list) + 2:
-        for star in range(sz+4):
-            print '*'
+putting_in_a_frame(s_list)
 
     
